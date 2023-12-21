a1b9d0b50cd0039f28e124cd4f0d0dc5ebcebab2
Sergei <szachesov@yandex.ru>
2023-12-21 18:14:15 +0300

    init commit

---
+++ src/main/java/ru/zachesov/learn/Main.java
@@ -0,0 +1,11 @@
+package ru.zachesov.learn;
+
+/**
+ * @author Zachesov Sergei
+ * @since 2023-12-15
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
